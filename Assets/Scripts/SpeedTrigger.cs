71e3116360d10afe4ff89b159d381d4778dc1f19
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-09-20 15:15:29 -0500

    lk

---
+++ Assets/Scripts/SpeedTrigger.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GravityTrigger : MonoBehaviour
+{
+    // Adjust the player's gravity scale
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Herbert"))
+        {
+            Rigidbody2D playerRigidbody = other.GetComponent<Rigidbody2D>();
+            if (playerRigidbody != null)
+            {
+                playerRigidbody.gravityScale *= 2; // Double the gravity
+            }
+        }
+    }
+}
