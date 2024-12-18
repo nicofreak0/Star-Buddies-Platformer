8f6962fb4c5693036b1ac9d6eb17a6e2212d5d9d
Lydia <picklesandlydia@gmail.com>
2024-12-17 18:51:23 -0600

    j

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,7 +9,7 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(1);
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(1);
     }
 
     public void QuitGame()
