05fd021112e51c6aab868d6fddb08a9780bd70b3
Lydia <146027704+nicofreak0@users.noreply.github.com>
2025-01-06 15:04:06 -0600

    sound slider and script

---
+++ Assets/Scripts/PlayQuickSound.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+/// <summary>
+/// Play a simple sounds using Play one shot with volume, and pitch
+/// </summary>
+[RequireComponent(typeof(AudioSource))]
+public class PlayQuickSound2D : MonoBehaviour
+{
+    [Tooltip("The sound that is played")]
+    public AudioClip sound = null;
+
+    [Tooltip("The volume of the sound")]
+    public float volume = 1.0f;
+
+    [Tooltip("The range of pitch the sound is played at (-pitch, pitch)")]
+    [Range(0, 1)] public float randomPitchVariance = 0.0f;
+
+    private AudioSource audioSource = null;
+
+    private float defaultPitch = 1.0f;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.spatialBlend = 0f; // Set to 2D
+    }
+
+    public void Play()
+    {
+        float randomVariance = Random.Range(-randomPitchVariance, randomPitchVariance);
+        randomVariance += defaultPitch;
+
+        audioSource.pitch = randomVariance;
+        audioSource.PlayOneShot(sound, volume);
+        audioSource.pitch = defaultPitch;
+    }
+
+    private void OnValidate()
+    {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+    }
+}
