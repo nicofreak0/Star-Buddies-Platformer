71974bd1234650ab2925a0e1878ac84927bd1e9f
Lydia <146027704+nicofreak0@users.noreply.github.com>
2025-02-07 14:14:18 -0600

    scroll

--- Assets/Scripts/CreditsScroll.cs
+++ Assets/Scripts/CreditsScroll.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class CreditsScroll : MonoBehaviour
 {
+
+    public float moveSpeed = 100f;
+    private bool hasWaited;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Invoke("Waited", 1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (hasWaited)
+            transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
+    }
+
+    void Waited()
+    {
+        hasWaited = true;
+
     }
 }
